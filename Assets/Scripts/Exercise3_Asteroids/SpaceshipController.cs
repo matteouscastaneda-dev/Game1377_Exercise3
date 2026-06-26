ecead90b248a341b4bd3b8910c94484638f5f0f5
MatteousCastaneda <matteous.castaneda@g.austincc.edu>
2026-06-26 12:58:08 -0500

    Updated SpaceshipController.cs to handle teleport
    Used the screenbounds variables from the screenbounds class to get a random location for the spaceship to change to. Changed the input key in unity so that fire2 is left shift.

--- Assets/Scripts/Exercise3_Asteroids/SpaceshipController.cs
+++ Assets/Scripts/Exercise3_Asteroids/SpaceshipController.cs
@@ -7,12 +7,12 @@
  * 
  * Requirements:
  * PART 1: Player Movement
- * 1. The player should be able to rotate the ship left and right using A/D keys from an input axis. (DONE)
+ * 1.(DONE) The player should be able to rotate the ship left and right using A/D keys from an input axis.
  *      This movement should be done with Transform based movement. 
- * 2. The player should be able to thrust forward using only the W key from an input axis. (DONE)
+ * 2. (DONE)The player should be able to thrust forward using only the W key from an input axis.
  *      This movement should be done with physics applied to a RigidBody2D. 
- * 3. The player should be able to wrap around the screen when they go off one edge and come back on the other side.
- * 4. The player should be able to teleport to a random location on the screen using left shift in an input button. You 
+ * 3. (DONE) The player should be able to wrap around the screen when they go off one edge and come back on the other side.
+ * 4. (DONE) The player should be able to teleport to a random location on the screen using left shift in an input button. You   
  *      do not need to check if there is an asteroid there. 
  *      Hint: For determining the random location, you can use the ScreenBounds class (see ScreenWrap.cs for how to use)
  *      
@@ -92,6 +92,9 @@ public class AsteroidsPlayerController : MonoBehaviour
 
     private void TeleportToRandomLocation()
     {
+        float randomLocationX = Random.Range(ScreenBounds.ScreenLeft, ScreenBounds.ScreenRight);
+        float randomLocationY = Random.Range(ScreenBounds.ScreenBottom, ScreenBounds.ScreenTop);
 
+        transform.position = new Vector2(randomLocationX, randomLocationY);
     }
 }
